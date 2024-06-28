d3c07889d9bf7e6f53514ed3de1b5bd3cc2f13a8
Carlos C Gomez. PhD <149920630+gomezphd@users.noreply.github.com>
2024-06-27 21:07:10 -0400

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,13 +1,17 @@
 
+// Wait for the DOM to fully load before running scripts
 document.addEventListener('DOMContentLoaded', function () {
+    // Select the hamburger menu icon and navigation links
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('nav ul');
     const navItems = document.querySelectorAll('nav ul li a');
 
+    // Add click event listener to toggle the visibility of navigation links
     hamburger.addEventListener('click', function () {
         navLinks.classList.toggle('show');
     });
 
+    // Add click event listeners to navigation items to hide the menu on click (for mobile view)
     navItems.forEach(item => {
         item.addEventListener('click', function () {
             if (navLinks.classList.contains('show')) {
